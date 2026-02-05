0356c82df1979aff2da6b77438dc37572ac3835a
TheJanitor1097 <247629573+TheJanitor1097@users.noreply.github.com>
2026-02-05 11:40:46 -0500

    Agrgando nuevas funciones.

--- tareas.py
+++ tareas.py
@@ -3,6 +3,32 @@ import os
 
 ARCHIVO_TAREAS = "tareas.json"
 
+def agregar_tarea(tareas):
+    titulo = input("\nEscribe el título de la tarea: ")
+    nueva_tarea = {
+        "id": len(tareas) + 1,
+        "titulo": titulo,
+        "completada": False
+    }
+    tareas.append(nueva_tarea)
+    guardar_tareas(tareas)
+    print("Tarea agregada correctamente.")
+
+
+def completar_tarea(tareas):
+    mostrar_tareas(tareas)
+    try:
+        tarea_id = int(input("\nIngresa el ID de la tarea completada: "))
+        for tarea in tareas:
+            if tarea["id"] == tarea_id:
+                tarea["completada"] = True
+                guardar_tareas(tareas)
+                print("Tarea marcada como completada.")
+                return
+        print("ID no encontrado.")
+    except ValueError:
+        print("Ingresa un número válido.")
+
 
 def cargar_tareas():
     if os.path.exists(ARCHIVO_TAREAS):
